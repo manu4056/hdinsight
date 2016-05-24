937833e756f914336169e300926e9f8259f34380
manu agrawal <manu4056@users.noreply.github.com>
2016-05-24 21:04:18 +0530

    Create packages.scala

---
+++ packages.scala
@@ -0,0 +1,10 @@
+// to import all types like (struct type , struct field..e.t.c)
+import org.apache.spark.sql.types._
+
+// to import row function
+import org.apache.spark.sql.Row;
+
+// to imoprt all sql functions
+
+import org.apache.spark.sql.functions.{concat, lit}
+
